eab535de12ef0fed82506e7c5da86be6d683e22a
Gab1nh0 <gabriel507xs@gmail.com>
2025-01-18 19:44:17 -0500

    nuevos proyectos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,9 +82,46 @@ export default function Home() {
 
 
         <div className={styles.section_titulo} id='proyectos'><h1>📋Proyectos</h1></div><br /><br /><br />
+        <div className={styles.proyecto_content}>
 
+            <div className={styles.proyecto_nombre}>
+                <h3>IBM Capstone Project</h3>
+                <div className={styles.proyecto_nombre_tabla}>
+                    <br />
+                    <table>
+                        <tr>
+                            <td>IBM Cognos</td>
+                            <td>Python</td>
+                        </tr>
+
+                        <tr>
+                            <td>Web Scrap</td>
+                            <td>Matplotlib</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div className={styles.proyecto_descripcion}>
+                <p>A través del análisis de encuestas a desarrolladores, este proyecto revela las herramientas y lenguajes más utilizados hoy y las tendencias emergentes que transformarán el panorama tecnológico.</p>    
+            </div>
+            
+            <div className={styles.proyecto_botones}>
+                <a href="https://github.com/Gab1nh0/IBM-Capstone-Project/blob/main/CapstoneProjectDashboard.pdf" target="_blank">Ver Dashboard <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 20 20"><path fill="currentColor" d="M17 17H3V3h5V1H3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5h-2z"/><path fill="currentColor" d="m11 1l3.29 3.29l-5.73 5.73l1.42 1.42l5.73-5.73L19 9V1z"/></svg></a>
+                <a href="https://github.com/Gab1nh0/IBM-Capstone-Project/blob/main/Final-capstone-story-template.pdf" target="_blank">Ver Análisis <svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 20 20"><path fill="currentColor" d="M17 17H3V3h5V1H3a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5h-2z"/><path fill="currentColor" d="m11 1l3.29 3.29l-5.73 5.73l1.42 1.42l5.73-5.73L19 9V1z"/></svg></a>
+            
+            </div>
+        </div><br /><br />
+        <div className={styles.proyecto_content_img}>
+            <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1737246961/zkysl0dsvttuzrho09ow.png" alt="" />
+            <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1737246961/xgzpb2gyv7roxlwtxblt.png" alt="" />
+            <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1737246961/wgmq29jabfufyiwouvvc.png" alt="" />
+        </div>
+
+        <br /><br /><br /><br /><hr /><br /><br /><br />
 
         <div className={styles.proyecto_content}>
+
             <div className={styles.proyecto_nombre}>
                 <h3>ML Football League.</h3>
                 <div className={styles.proyecto_nombre_tabla}>
@@ -220,6 +257,42 @@ export default function Home() {
             <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1723959238/p1un3g96iaxkonlfryol.png" alt="" />
             <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1723959238/vcksgfuhpmwa5rmfwihv.png" alt="" />
             <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1723959238/tulyopxxarme43meynwh.png" alt="" />
+        </div>
+
+        <br /><br /><br /><br /><hr /><br /><br /><br />
+
+
+        <div className={styles.proyecto_content}>
+            <div className={styles.proyecto_nombre}>
+                <h3>DigestiLearn</h3>
+                <div className={styles.proyecto_nombre_tabla}>
+                    <br />
+                    <table>
+                        <tr>
+                            <td>Java</td>
+                            <td>Android Studio</td>
+                        </tr>
+
+                        <tr>
+                            <td>Figma</td>
+                            <td>Quiz</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div className={styles.proyecto_descripcion}>
+                <p>App para aprender sobre el cuerpo humano con este interactivo drag & drop diseñado para niños. Aprendiendo cómo funciona el sistema digestivo mientras colocamos los órganos en su lugar. Educación y diversión para niños</p>
+            </div>
+            
+            <div className={styles.proyecto_botones}>
+                <a href="https://github.com/Gab1nh0/manejo-de-elementos-graficos" target="_blank">Ver Repositorio <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/></svg></a>
+            </div>
+        </div><br /><br />
+        <div className={styles.proyecto_content_img}>
+            <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1737246548/d3iiaspuyzklcrq7o4s1.jpg" alt="" />
+            <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1737246548/ruir4sm9xo9l9ka0myho.jpg" alt="" />
+            <img className={styles.img} src="https://res.cloudinary.com/dnvwjkcpj/image/upload/v1737246548/ncdo8id1fzqje54azn2e.jpg" alt="" />
         </div>
 
         <br /><br /><br /><br /><hr /><br /><br /><br />
